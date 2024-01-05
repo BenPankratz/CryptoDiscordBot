9fa3088236475559c310013d697d4a993aac6e2e
Ben Pankratz <105238108+BenPankratz@users.noreply.github.com>
2024-01-05 13:25:02 -0600

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -5,16 +5,18 @@ intents = discord.Intents.default()
 intents.messages = True 
 intents.message_content = True
 
-# Replace 'your_bot_token' with the token you got from the Discord Developer Portal
+
 TOKEN = 'MTE5Mjg4OTc0MDQ1ODk5NTc2NA.GWBCSm.KvdD_qaVkrn3Yy0sI-a0m2Vjb5T5rYpelPaAPw'
 
 # Prefix for commands
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+# Intitialize bot
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
+# Bot Commands
 @bot.command(name='test')
 async def test(ctx):
     print("Test command was called")
