1555b3707889823aea87009c452e7c3334e1a537
Ben Pankratz <105238108+BenPankratz@users.noreply.github.com>
2024-01-12 12:29:08 -0600

    Add crypto command

--- bot/main.py
+++ bot/main.py
@@ -1,38 +1,58 @@
 import discord
 from discord.ext import commands
+import requests
+from bs4 import BeautifulSoup
 
-class MyBot(commands.Bot):
-    #event handling
-    async def on_ready(self):
-        print(f'{self.user.name} has connected to Discord!')
-
-    #command handling
-    @commands.command(name='test')
-    async def test(self, ctx):
-        print("Test command was called")
-        await ctx.send('Bot is up and running!')
-
-    @commands.command(name='beast')
-    async def beast(self, ctx):
-        await ctx.send('You sexy beast!')
-
-    @commands.command(name='HowDoesDanLikeHisWomen')
-    async def HowDoesDanLikeHisWomen(self, ctx):
-        await ctx.send('Large!') 
-
-    @commands.command(name='product')
-    async def product(self, ctx):
-        embed = discord.Embed(title="Product Name", description="This is a great product.", color=0x00ff00)
-        embed.add_field(name="Price", value="$9.99")
-        embed.set_image(url="https://upload.wikimedia.org/wikipedia/en/thumb/0/09/Skibidi_toilet_screenshot.webp/237px-Skibidi_toilet_screenshot.webp.png")
-        await ctx.send(embed=embed)
-
-#Setup the bot
 intents = discord.Intents.default()
-intents.messages = True 
+intents.messages = True
 intents.message_content = True
-TOKEN = 'replace with TOKEN' #Delete token when uploading to GitHub
 
-#Start and initialize the bot
-bot = MyBot(command_prefix='!', intents=intents)
+TOKEN = 'Replace with token' #Delete token when uploading to GitHub
+
+API_KEY = 'CG-6Jo2ZVRrgQBFywfd3BVoEWKZ'
+
+# Prefix for commands
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Intitialize bot
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+# Bot Commands
+@bot.command(name='test')
+async def test(ctx):
+    print("Test command was called")
+    await ctx.send('Bot is up and running!')
+
+
+def get_crypto_price(coin_id):
+    url = 'https://api.coingecko.com/api/v3/simple/price'
+    print(coin_id)
+    params = {  
+        'ids': coin_id,
+        'vs_currencies': 'usd'
+    }
+    response = requests.get(url, params = params)
+    data = response.json()
+    return data[coin_id]['usd'] if coin_id in data else None
+
+@bot.command(name='crypto')
+async def crypto(ctx, coin):
+    price = get_crypto_price(coin.lower())
+    if price is not None:
+        await ctx.send(f"The current price of {coin.upper()} is: ${price}")
+    else:
+        await ctx.send(f"Couldn't fetch price for {coin.upper()}. Please check the coin symbol.")
+
+
+@bot.command(name='beast')
+async def beast(ctx):
+    await ctx.send('You sexy beast!')
+
+@bot.command(name='HowDoesDanLikeHisWomen')
+async def HowDoesDanLikeHisWomen(ctx):
+    await ctx.send('Large!')
+
+# Start the bot
 bot.run(TOKEN)
