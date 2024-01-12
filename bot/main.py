bedae06a0caed59bf7eb7cdcac6c32e95c1d2535
Dan Hawker <dhawk064@gmail.com>
2024-01-12 12:35:59 -0600

    tickers

--- bot/main.py
+++ bot/main.py
@@ -1,13 +1,12 @@
 import discord
 from discord.ext import commands
 import requests
-from bs4 import BeautifulSoup
 
 intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
 
-TOKEN = 'Replace with token' #Delete token when uploading to GitHub
+TOKEN = 'MTE5Mjg4OTc0MDQ1ODk5NTc2NA.GfN6ZX.jlwYkBNiztrcJwoseEMAJvKOJjZougnLDH2KBU' #Delete token when uploading to GitHub
 
 API_KEY = 'CG-6Jo2ZVRrgQBFywfd3BVoEWKZ'
 
@@ -25,10 +24,19 @@ async def test(ctx):
     print("Test command was called")
     await ctx.send('Bot is up and running!')
 
+COIN_IDS = {
+    'btc': 'bitcoin',
+    'eth': 'ethereum',
+    'xrp': 'ripple',
+    # Add more mappings as needed
+}
 
-def get_crypto_price(coin_id):
+def get_crypto_price(ticker):
+    coin_id = COIN_IDS.get(ticker.lower())
+    if not coin_id:
+        return None
+    
     url = 'https://api.coingecko.com/api/v3/simple/price'
-    print(coin_id)
     params = {  
         'ids': coin_id,
         'vs_currencies': 'usd'
