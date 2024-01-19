0a02f31ef68da0bddc1d65a8416e089e4ddccfea
Ben Pankratz <105238108+BenPankratz@users.noreply.github.com>
2024-01-18 19:13:09 -0600

    add function

--- bot/database.py
+++ bot/database.py
@@ -8,9 +8,21 @@ def initialize_database():
     conn.commit()
     conn.close()
 
-def insert_data(name, age):
-    # Insert data into the database
-    # ...
+def insert_coin(username, coin):
+    conn = sqlite3.connect('my_database.db')
+    cursor = conn.cursor()
+
+    # Check if the user already has a coin set
+    cursor.execute("SELECT * FROM user_coins WHERE username = ?", (username))
+    if cursor.fetchone():
+        # Update the existing coin
+        cursor.execute("UPDATE user_coins SET coin = ? WHERE username = ?", (coin, username))
+    else:
+        # Insert new user and coin
+        cursor.execute("INSERT INTO user_coins (username, coin) VALUES (?, ?)", (username, coin))
+
+    conn.commit()
+    conn.close()
 
 def query_data():
     # Query data from the database
